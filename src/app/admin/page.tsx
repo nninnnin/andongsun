5f44e64ceff426d1f67068c8f0a764bce8897bb7
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2024-08-20 01:49:09 +0900

    Add admin page and set up quill.js

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import clsx from "clsx";
+import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const AdminPage = () => {
+  return (
+    <div
+      className={clsx(
+        "bg-violet-200 w-screen h-screen",
+        "flex justify-center items-center"
+      )}
+    >
+      <ReactQuill
+        className={clsx(
+          "w-[50vw] min-h-[50vh] bg-white flex flex-col"
+        )}
+      />
+    </div>
+  );
+};
+
+export default AdminPage;

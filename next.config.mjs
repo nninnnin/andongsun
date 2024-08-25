3784cd7d00a75649c439cd70348e54fac85ff5d2
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2024-08-25 11:56:51 +0900

    admin: 이미지첨부 기능 완성, 컴포넌트 분리중

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  env: {
+    MEMEX_TOKEN: process.env.MEMEX_TOKEN,
+  },
+};
 
 export default nextConfig;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import CategorySelect from "@/components/admin/CategorySelect";
 import clsx from "clsx";
 import dynamic from "next/dynamic";
-import React, { useMemo } from "react";
+import React, { ChangeEvent, useMemo } from "react";
+import Mf from "@rebel9/memex-fetcher";
+
+const memexFetcher = Mf.createMemexFetcher(
+  process.env.MEMEX_TOKEN ?? ""
+);
+
+import CategorySelect from "@/components/admin/CategorySelect";
 import "react-quill/dist/quill.snow.css";
 
 const AdminPage = () => {
@@ -29,6 +35,56 @@ const AdminPage = () => {
           "w-[50vw] min-h-[50vh] bg-white flex flex-col"
         )}
       />
+
+      <AdminPage.MainImage />
+    </div>
+  );
+};
+
+AdminPage.Row = ({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className={clsx("flex flex-col")}>
+      <label>{label}</label>
+      {children}
+    </div>
+  );
+};
+
+AdminPage.MainImage = () => {
+  const [selectedImage, setSelectedImage] =
+    React.useState<File | null>(null);
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(
+          e: ChangeEvent<HTMLInputElement>
+        ) => {
+          console.log(e.target.files);
+
+          setSelectedImage(
+            e.target.files?.[0] ?? null
+          );
+        }}
+      />
+      <button
+        disabled={!selectedImage}
+        onClick={async () =>
+          await memexFetcher.postMedia(
+            "cbbcc6cd",
+            selectedImage!
+          )
+        }
+      >
+        이미지 저장하기
+      </button>
     </div>
   );
 };
